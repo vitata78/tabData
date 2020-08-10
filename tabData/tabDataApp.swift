455d8429d134f98bd332dd1788f958712b923121
Andrew Tolstov <andrew.tolstov@gmail.com>
2020-08-10 00:33:21 -0400

    Initial Commit

---
+++ tabData/tabDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabDataApp.swift
+//  tabData
+//
+//  Created by Andrew Tolstov on 8/10/20.
+//
+
+import SwiftUI
+
+@main
+struct tabDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
